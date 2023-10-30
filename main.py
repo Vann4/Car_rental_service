fef4b033cdb8716d72d14a5e9a52d778fbfdda67
Vann4 <1vetlan1@mail.ru>
2023-10-30 21:01:42 +0300

    Аутентификация

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status, Request, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -29,7 +29,7 @@ def get_db():
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/Account/SignIn")
 
 
 class Token(BaseModel):
@@ -58,10 +58,6 @@ def get_password_hash(password):
 
 
 def get_user(username: str, db: Session = next(get_db())):
-    # user_db = db.query(User).filter(models.User.username == username).first()
-    # if username in user_db:
-    #     user_dict = user_db[username]
-    #     return UserInDB(**user_dict)
     return crud.get_user_by_username(db=db, username=username)
 
 
@@ -113,10 +109,13 @@ async def get_current_active_user(
     return current_user
 
 
-@app.post("/token", response_model=Token)
-async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
+@app.get("/api/Account/Me")
+async def read_account_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
+
+
+@app.post("/api/Account/SignIn", response_model=Token)
+async def login_for_access_token(response: Response, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = authenticate_user(form_data.username, form_data.password)
     if not user:
         raise HTTPException(
@@ -132,55 +131,15 @@ async def login_for_access_token(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@app.get("/api/Account/SignIn")
-async def read_account_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return current_user
-
-
-@app.get("/users/me/rent/")
-async def read_own_items(
-    current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-@app.post("/user/", response_model=schemas.User)
+@app.post("/api/Account/SignUp", response_model=schemas.User)
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_username(db, username=user.username)
     if db_user:
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(status_code=400, detail="Такой пользователь уже существует")
     return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=list[schemas.User])
-async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+async def read_users(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/api/Transport", response_model=schemas.Transport)
-async def create_transport(transport: schemas.TransportCreate, db: Session = Depends(get_db)):
-    return crud.create_transport(db=db, transport=transport)
-
-
-@app.post("/api/Admin/Rent/", response_model=schemas.Rent)
-async def create_rent_for_user(rent: schemas.RentCreate, db: Session = Depends(get_db)):
-    return crud.create_user_rent(db=db, rent=rent)
-
-
-@app.get("/api/Admin/Rent/{rentId}", response_model=list[schemas.Rent])
-async def read_rents(rentId: int, db: Session = Depends(get_db)):
-    rent = crud.get_rents(db, rentId=rentId)
-    return rent
-
-
